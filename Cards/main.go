ef95a767937c245a6f817f1cdc9665b60d63e2a7
Esra <esergiser@gmail.com>
2023-07-19 16:09:17 +0300

    deck type oluşturuldu

--- Cards/main.go
+++ Cards/main.go
@@ -1,18 +1,14 @@
 package main
 
-import "fmt"
-
 func main(){
 	//var card string = "Ace of Spades" equals this:
-	cards := []string{"Ace of Diamonds", newCard()}
+	cards := deck{"Ace of Diamonds", newCard()}
 	cards = append(cards,"Six of Spades")
 
-	for i, card := range cards {
-		fmt.Println(i, card)
-	}
-
+	cards.print()
 
 	
+
 }
 
 func newCard() string {
