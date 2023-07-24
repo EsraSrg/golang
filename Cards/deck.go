082148c1096c908969006dd22dd74e522f60fcfe
Esra <esergiser@gmail.com>
2023-07-24 17:04:58 +0300

    Error handling

--- Cards/deck.go
+++ Cards/deck.go
@@ -3,48 +3,46 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"os"
 	"strings"
-
 )
 
 // create a new type of 'deck'
 // which is a slice of strings
-type deck []string 
+type deck []string
 
-func newDeck () deck {  // anytime someone calls newDeck ,they're gonna return a value of type deck.
-	cards:= deck{}
+func newDeck() deck { // anytime someone calls newDeck ,they're gonna return a value of type deck.
+	cards := deck{}
 
 	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
-	cardValues:= []string{"Ace","Two", "Three", "Four",}
+	cardValues := []string{"Ace", "Two", "Three", "Four"}
 
-	for _, suit := range cardSuits{
-		for _,value := range cardValues{
-			cards = append(cards , value+ " of " +suit)
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, value+" of "+suit)
 		}
 	}
 	return cards
 
 }
 
-func (d deck) print() {  // receiver argument
-	for i , card := range d {
+func (d deck) print() { // receiver argument
+	for i, card := range d {
 		fmt.Println(i, card)
 	}
 }
 
-func deal(d deck, handSize int) (deck , deck){
+func deal(d deck, handSize int) (deck, deck) {
 
-	return d[:handSize],d[handSize:]
-
+	return d[:handSize], d[handSize:]
 
 }
 
 func (d deck) toString() string { //receiver type deck
-	return strings.Join ([]string(d) , ",")
+	return strings.Join([]string(d), ",")
 	//[]string(d) type conversion : deck become slice of string
 	// Strings
 
-	
 }
 
 func (d deck) saveToFile(filename string) error {
@@ -52,6 +50,15 @@ func (d deck) saveToFile(filename string) error {
 }
 
 func newDeckFromFile(filename string) deck {
-	 bs, err := ioutil.ReadFile(filename)
+	bs, err := ioutil.ReadFile(filename)
+	if err != nil {
+		// Option #1 - log the error and return a call to newDeck()
+		// Option #2 - log the error and entirely quit the program
+		fmt.Println("Error:", err)
+		os.Exit(1)
+	}
+
+	s := strings.Split(string(bs), ",")
+	return deck(s)
+
 }
- 
