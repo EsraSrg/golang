859af23ba9e90c91dbf0a2a59c135f82ddf62fda
Esra <esergiser@gmail.com>
2023-07-28 12:36:58 +0300

    copy() func in slices

--- practise/slice.go
+++ practise/slice.go
@@ -5,17 +5,34 @@ import (
 )
 
 func main() {
-	myslice1 := []int{1, 2, 3, 4, 5, 6}
-	fmt.Printf("myslice1 = %v\n", myslice1)
-	fmt.Printf("length = %d\n", len(myslice1))
-	fmt.Printf("capacity = %d\n", cap(myslice1))
+	numbers := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15}
+	// Original slice
+	fmt.Printf("numbers = %v\n", numbers)
+	fmt.Printf("length = %d\n", len(numbers))
+	fmt.Printf("capacity = %d\n", cap(numbers))
 
-	myslice1 = append(myslice1, 20, 21)
-	fmt.Printf("myslice1 = %v\n", myslice1)
-	fmt.Printf("length = %d\n", len(myslice1))
-	fmt.Printf("capacity = %d\n", cap(myslice1))
+	// Create copy with only needed numbers
+	neededNumbers := numbers[:len(numbers)-10]
+	numbersCopy := make([]int, len(neededNumbers))
+	copy(numbersCopy, neededNumbers)
 
+	fmt.Printf("numbersCopy = %v\n", numbersCopy)
+	fmt.Printf("length = %d\n", len(numbersCopy))
+	fmt.Printf("capacity = %d\n", cap(numbersCopy))
 }
+
+// func main() {
+// 	myslice1 := []int{1, 2, 3, 4, 5, 6}
+// 	fmt.Printf("myslice1 = %v\n", myslice1)
+// 	fmt.Printf("length = %d\n", len(myslice1))
+// 	fmt.Printf("capacity = %d\n", cap(myslice1))
+
+// 	myslice1 = append(myslice1, 20, 21)
+// 	fmt.Printf("myslice1 = %v\n", myslice1)
+// 	fmt.Printf("length = %d\n", len(myslice1))
+// 	fmt.Printf("capacity = %d\n", cap(myslice1))
+
+// }
 
 // func main() {
 // 	arr1 := [6]int{10, 11, 12, 13, 14, 15}
