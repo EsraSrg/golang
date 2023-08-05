5a13d7c3b755f8a11827b972a30f849574176c44
Esra <esergiser@gmail.com>
2023-08-06 02:37:44 +0300

    printing site status

--- channels/main.go
+++ channels/main.go
@@ -9,22 +9,25 @@ func main() {
 	links := []string{
 		"http://google.com",
 		"http://facebook.com",
-		"http://stacoverflow.com",
+		"http://stackoverflow.com",
 		"http://golang.org",
 		"http://amazon.com",
 	}
-
 	for _, link := range links {
-
+		checkLink(link)
 	}
-
 }
 
 func checkLink(link string) {
 	_, err := http.Get(link)
 	if err != nil {
-		fmt.Println(link + "might be down!")
+		fmt.Println(link + " might be down!")
 		return
 	}
-	fmt.Prinltn(link, "is up")
+	fmt.Println(link, "is up")
 }
+
+
+
+
+
