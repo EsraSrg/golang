7cb782c5d054c231aa230e81744b0d3b594b104a
Esra <esergiser@gmail.com>
2023-08-06 04:19:38 +0300

    sleeping a routine and func literals

--- channels/main.go
+++ channels/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
 )
 
 func main() {
@@ -21,7 +22,11 @@ func main() {
 	}
 	
 	for l:= range c {
+		go func(){  //function literal
+
+		time.Sleep(5 * time.Second)
 		go checkLink(l,c)
+		}()
 	}
 	
 }
